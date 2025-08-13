3f762d2abcaff3346e333ac2753f3c5f0cc588fe
Mazenn3 <167587407+Mazenn3@users.noreply.github.com>
2025-08-13 07:54:55 +0300

    Update portfolio-data.js

--- portfolio-data.js
+++ portfolio-data.js
@@ -308,7 +308,7 @@ projects: {
     {
       name: "Urban Pulse: Divvy Bike Share Analytics Dashboard",
       image: "images/Bikeshare Windy City.jpg",
-      video: "images/Urban Pulse Divvy Bike Share Analytics Dashboard.png",
+      video: "images/Urban Pulse_ Divvy Bike Share Analytics Dashboard1.mp4",
       role: ["Processed and cleaned 205,691 bike trip records using Power Query",
              "Built comprehensive Power BI dashboard with 8 interactive visualizations",
              "Delivered actionable recommendations for fleet optimization and station management"
